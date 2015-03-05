eafeeabf714048e9f4be4ddfa864e2873aa139a6
Murthy10 <samuel.kurath@gmail.com>
2015-03-05 12:19:12 +0100

    with second1

--- luesis/js/impression.js
+++ luesis/js/impression.js
@@ -1,10 +1,8 @@
 var dir = "img/impressions/";
 var fileextension = ".jpg";
 $.ajax({
-    //This will retrieve the contents of the folder if the folder is configured as 'browsable'
     url: dir,
     success: function (data) {
-        //Lsit all jpg file names in the page
         $(data).find("a:contains(" + fileextension + ")").each(function () {
             var filename = this.href.replace(window.location.host, "").replace("http:///", "");
             $(".carousel-inner").append($("<div class=\"item active\"><img src=" + dir + filename + "></img></div>"));
